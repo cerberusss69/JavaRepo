01dee3d6ead7bd0e46a4f037747d31aa5fd98e43
Harry <Lilcity@mail.ru>
2022-02-04 00:20:10 +0400

    дз№1

---
+++ HomeWorkApp.java
@@ -0,0 +1,60 @@
+package urok1;
+// Задание 1
+public class HomeWorkApp {
+//    public static void main (String[] args){}
+
+// Задание 2
+//    public static void main (String [] args) {
+//        printThreeWords();
+//    }
+//    public static void printThreeWords() {
+//        System.out.println("Orange");
+//        System.out.println("Banana");
+//        System.out.println("Apple");
+//    }
+
+//    Задание 3
+//    public static void main (String[]args) {
+//        checkSumSign();
+//    }
+//    public static void checkSumSign() {
+//        int a = 5, b = 10, c = 0;
+//         c = a + b;
+//         System.out.println("Результат " + c);
+//         if (c >= 0) {
+//             System.out.println("Сумма положительная");
+//         } if (c <= 0) {
+//             System.out.println("Cумма отрицательная");
+//        }
+//    }
+
+//    Задание 4
+//    public static void main (String [] args) {
+//        printColor();
+//    }
+//    public static void printColor () {
+//
+//        int value = 501;
+//            if (value <= 0) {
+//                System.out.println("Красный");
+//            } if (value > 0 && value <= 100) {
+//                System.out.println("Желтый");
+//            } if (value > 100) {
+//                System.out.println("Зеленый");
+//            }
+//        }
+
+//    Задание 5
+    public static void main (String [] args) {
+        compareNumbers();
+    }
+    public static void compareNumbers() {
+        int a = 10;
+        int b = 5;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } if (a < b) {
+            System.out.println("a < b");
+        }
+    }
+}
